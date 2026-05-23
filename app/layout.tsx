6319d53ae6acfc73f3c4df060dee2596440e367b
duckettjoe-eng <duckettjoe@gmail.com>
2026-05-23 13:26:15 -0700

    add pwa support

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,12 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Murphys Community Radio",
   description: "Amplifying the voices of Calaveras County",
+  manifest: "/manifest.json",
+  themeColor: "#c9a227",
+  icons: {
+    icon: "/icon-192.png",
+    apple: "/icon-512.png",
+  },
 };
 
 export default function RootLayout({
